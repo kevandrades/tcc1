acb1d216149770f4c39b9b5cc6d4eaf70a5d5a52
Kevyn Andrade <kevsdeandrade@gmail.com>
2023-06-20 16:47:42 -0300

    formatação

--- example/quantreg.R
+++ example/quantreg.R
@@ -2,89 +2,89 @@ if (!require(pacman)) install.packages("pacman")
 
 pacman::p_load(quantreg, ggplot2)
 
-create_df <- function(n, std, add_outliers=FALSE) {
-     set.seed(299792458)
+create_df <- function(n, std, add_outliers = FALSE) {
+  set.seed(299792458)
 
-     x <- runif(n, 0, 7)
+  x <- runif(n, 0, 7)
 
-     y <- 100 - 27 * x + rnorm(n, sd = std)
+  y <- 100 - 27 * x + rnorm(n, sd = std)
 
-     flag <- rep("common", n)
+  flag <- rep("common", n)
 
-     if (add_outliers) {
-       xout <- runif(3, 6, 7)
+  if (add_outliers) {
+    xout <- runif(3, 6, 7)
 
-       yout <- rnorm(3, 97)
+    yout <- rnorm(3, 97)
 
-       flagout <- rep("outlier", 3)
+    flagout <- rep("outlier", 3)
 
-       x <- c(x, xout)
+    x <- c(x, xout)
 
-       y <- c(y, yout)
+    y <- c(y, yout)
 
-       flag <- c(flag, flagout)
-     }
+    flag <- c(flag, flagout)
+  }
 
-     return(data.frame(x, y, flag))
+  return(data.frame(x, y, flag))
 }
 
 draw_vert_dist <- function(x0, model) {
-     std <- sigma(lm_fit)
-     coeffs <- coef(lm_fit)
+  std <- sigma(lm_fit)
+  coeffs <- coef(lm_fit)
 
-     a <- coeffs[1]
-     b <- coeffs[2]
+  a <- coeffs[1]
+  b <- coeffs[2]
 
-     y0 <- a + b * x0
+  y0 <- a + b * x0
 
-     segment <- data.frame(
-          x = x0, y = y0 - (2 * std),
-          xend = x0, yend = y0 + 2 * std
-     )
+  segment <- data.frame(
+    x = x0, y = y0 - (2 * std),
+    xend = x0, yend = y0 + 2 * std
+  )
 
-     x <- seq(-2*std, 2*std, length.out = 50)
-     y <- dnorm(x, 0, std/1.5)/dnorm(0, 0, std/1.5)
+  x <- seq(-2 * std, 2 * std, length.out = 50)
+  y <- dnorm(x, 0, std / 1.5) / dnorm(0, 0, std / 1.5)
 
-     path <- data.frame(x = y + x0, y = x + y0)
+  path <- data.frame(x = y + x0, y = x + y0)
 
 
-     return(
-          list(
-               geom_path(aes(x, y), data = path),
-               geom_segment(
-                    aes(x=x, y=y, xend=xend, yend=yend),
-                    data = segment
-               )
-          )
-     )
+  return(
+    list(
+      geom_path(aes(x, y), data = path),
+      geom_segment(
+        aes(x = x, y = y, xend = xend, yend = yend),
+        data = segment
+      )
+    )
+  )
 }
 
 std <- 30
 
-df <- create_df(n = 27, std=std)
+df <- create_df(n = 27, std = std)
 
-df_out <- create_df(n = 27, std=std, add_outliers=TRUE)
+df_out <- create_df(n = 27, std = std, add_outliers = TRUE)
 
 lm_fit <- lm(y ~ x, data = df)
 
-scatterplot <- ggplot(df, mapping = aes(x=x, y=y)) +
-    geom_point() +
-    labs(
-      title = "Modelos de regressão", subtitle = "Funcionamento geral",
-      x = "Variável explicativa", y = "Variável resposta"
-    ) +
-    theme_classic() +
-    theme(legend.position="none")
-
-
-scatterplot_out <- ggplot(df_out, mapping = aes(x=x, y=y)) +
-  geom_point(aes(color=flag)) +
+scatterplot <- ggplot(df, mapping = aes(x = x, y = y)) +
+  geom_point() +
   labs(
     title = "Modelos de regressão", subtitle = "Funcionamento geral",
     x = "Variável explicativa", y = "Variável resposta"
   ) +
   theme_classic() +
-  theme(legend.position="none")
+  theme(legend.position = "none")
+
+
+scatterplot_out <- ggplot(df_out, mapping = aes(x = x, y = y)) +
+  geom_point(aes(color = flag)) +
+  labs(
+    title = "Modelos de regressão", subtitle = "Funcionamento geral",
+    x = "Variável explicativa", y = "Variável resposta"
+  ) +
+  theme_classic() +
+  theme(legend.position = "none")
 
 scatterplot
 ggsave("example/img/scatter1.pdf")
@@ -100,24 +100,24 @@ ggsave("example/img/scatter2.pdf")
 
 
 scatterplot +
-  geom_smooth(method=lm, se=FALSE, color="red")
+  geom_smooth(method = lm, se = FALSE, color = "red")
 ggsave("example/img/scatter3.pdf")
 
 
 scatterplot +
-  geom_smooth(method=lm, se=FALSE, color="red") +
-  geom_quantile(quantiles=.5)
+  geom_smooth(method = lm, se = FALSE, color = "red") +
+  geom_quantile(quantiles = .5)
 ggsave("example/img/scatter4.pdf")
 
 
 scatterplot_out +
   geom_smooth(method = lm, se = FALSE, color = "red") +
-  geom_quantile(quantiles=.5) +
-  scale_color_manual(values=c("black", "red"))
+  geom_quantile(quantiles = .5) +
+  scale_color_manual(values = c("black", "red"))
 ggsave("example/img/scatter5.pdf")
 
 scatterplot_out +
   geom_smooth(method = lm, se = FALSE, color = "red") +
   geom_quantile() +
-  scale_color_manual(values=c("black", "red"))
+  scale_color_manual(values = c("black", "red"))
 ggsave("example/img/scatter6.pdf")
