02dcf82888f666a0fa1eacf7d2bc963a619f4f57
Kevyn Andrade <kevsdeandrade@gmail.com>
2024-03-24 01:21:05 -0300

    usando leitura com chunks

--- src/data/loading.py
+++ src/data/loading.py
@@ -1,6 +1,7 @@
 from .maps import FT_SELECT_COLUMNS, ID_FORMATO, SIGLA_GRAU, ORIGINARIO, NUM_EXP_COLUMNS
 from settings.generics import c
 import polars as pl
+import pandas as pd
 
 DTYPES = {
     "ultimo_dia": pl.Date,
@@ -8,24 +9,29 @@ DTYPES = {
 }
 
 def read_data(filename, columns=FT_SELECT_COLUMNS, dtypes=DTYPES):
-    df = (
-        pl.read_csv(filename, columns=columns, null_values=["NA"], ignore_errors = True, encoding="latin1", separator=";", dtypes=dtypes)
-        .filter(
-            (c.ramo_justica == "Justiça do Trabalho") &
-            (c.id_formato.is_in((1, 2)))
+    df = pl.concat([
+        (
+            pl.from_pandas(df)
+            .cast(dtypes=dtypes)
+            .filter(
+                (c.ramo_justica == "Justiça do Trabalho") &
+                (c.id_formato.is_in((1, 2)))
+            )
+            .with_columns(
+                c.originario.replace(ORIGINARIO),
+                formato = c.id_formato.replace(ID_FORMATO),
+                grau = c.sigla_grau.replace(SIGLA_GRAU),
+                *(pl.col(column).fill_null(0) for column in NUM_EXP_COLUMNS + ('ind1', ))
+            )
+            .with_columns(tramit_tmp = c.ind16_dias / c.ind16_proc)
+        ) for df in
+        pd.read_csv(filename, usecols=columns, na_values=["NA", ""],
+            encoding="latin1",
+            delimiter=";",
+            chunksize=10000
         )
-        .with_columns(
-            c.originario.map_dict(ORIGINARIO),
-            formato = c.id_formato.map_dict(ID_FORMATO),
-            grau = c.sigla_grau.map_dict(SIGLA_GRAU),
-            *{
-                column: pl.col(column).fill_null(0)
-                for column in columns
-                if "ind" in column and "min" not in column and "max" not in column
-            }
-        )
-        .with_columns(tramit_tmp = c.ind16_dias / c.ind16_proc)
-    )
+        
+    ])
 
     return df
 
