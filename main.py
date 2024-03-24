cea8883bb8f620f2ec63e34e1bfe748b27bd4669
Kevyn Andrade <kevsdeandrade@gmail.com>
2024-03-24 01:20:35 -0300

    modificando chamadas

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import polars as pl
-from src.data.loading import read_data, c
+import pandas as pd
+from src.data.loading import read_data, c, DTYPES, FT_SELECT_COLUMNS
 from src.charts import (
     ts_fmt, dist_tramit_tmp, grau_tempo,
     fmt_tmp, originario_tempo,
@@ -8,15 +9,18 @@ from src.charts import (
 # -----------------------------------------------
 
 # 1. Lendo os dados
-#df = read_data("data/tbl_fato_R.csv")
+#df = pl.read_csv(
+#    "data/tbl_ft_TRT.csv",
+#    null_values=["NA"],
+#    ignore_errors = True, encoding="utf-8",
+#    separator=",", dtypes=DTYPES
+#)#
 
-# -----------------------------------------------
-# 2. Plotando os gráficos
-# _______________________________
-# 2.1 Físicos ao longo do tempo
-#(ts_fmt_chart := ts_fmt(df))
-#
-#ts_fmt_chart.save("img/py/ts_fmt.pdf", width = 7, height = 4, limitsize=False)
+## -----------------------------------------------
+## 2. Plotando os gráficos
+## _______________________________
+## 2.1 Físicos ao longo do tempo
+#ts_fmt(df).save("img/py/ts_fmt.pdf", width = 7, height = 4, limitsize=False)
 
 # _______________________________
 # 2.2 Distribuição do tempo de tramitação
@@ -27,31 +31,21 @@ from src.charts import (
 
 df = pl.read_csv("data/ft_filtrado.csv")
 
-(dist_tramit_tmp_chart := dist_tramit_tmp(df))
-
-dist_tramit_tmp_chart.save("img/py/dist_tempo.pdf", width = 7, height = 4, limitsize=False)
+dist_tramit_tmp(df).save("img/py/dist_tempo.pdf", width = 7, height = 4, limitsize=False)
 
 # _______________________________
 # 2.3 Tempo de tramitação por formato
 
-(fmt_tmp_chart := fmt_tmp(df))
-
-fmt_tmp_chart.save("img/py/formato_tempo.pdf", width = 6, height = 3.75)
+fmt_tmp(df).save("img/py/formato_tempo.pdf", width = 6, height = 3.75)
 
 # _______________________________
 # 2.4 Tempo de tramitação por grau
-(grau_tempo_chart := grau_tempo(df))
-
-grau_tempo_chart.save("img/py/grau_tempo.pdf", width = 6, height = 3.75)
+grau_tempo(df).save("img/py/grau_tempo.pdf", width = 6, height = 3.75)
 
 # _______________________________
 # 2.4 Tempo de tramitação por originário
-(originario_tempo_chart := originario_tempo(df))
-
-originario_tempo_chart.save("img/py/originario_tempo.pdf", width = 6, height = 3.75)
+originario_tempo(df).save("img/py/originario_tempo.pdf", width = 6, height = 3.75)
 
 # _______________________________
 # 2.5 Tempo de tramitação por procedimento
-(prcdmnt_tmp_chart := procedimento_tempo(df))
-
-prcdmnt_tmp_chart.save("img/procedimento_tempo.pdf", width = 7, height = 4.6)
+procedimento_tempo(df).save("img/py/procedimento_tempo.pdf", width = 7, height = 4.6)
