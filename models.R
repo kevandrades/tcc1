e34359ad40e19f0885730527ffbe191d2567a846
Kevyn Andrade <kevsdeandrade@gmail.com>
2024-03-24 01:18:43 -0300

    corrigindo seleção

--- models.R
+++ models.R
@@ -1,42 +1,36 @@
 if (!require(pacman)) install.packages("pacman")
 pacman::p_load(data.table, tidyverse, tidymodels, quantreg, MASS, glue)
 
-servidores <- (
-    fread("data/servidores.csv", na.strings="-")
-    [, id_orgao_julgador := as.integer(id_orgao_julgador)]
-    [str_detect(sigla_tribunal, "TRT")]
-)
+set.seed(299792458)
+ft <- fread("data/infame_filter.csv") 
 
-ft <- fread("data/dummied.csv")
+ft_train <- ft %>%
+    sample_n(2000)
 
-ft[, id_formato := c(0, 1)[id_formato]]
+explicative_columns <- names(ft) %>%
+    setdiff("tramit_tmp") %>%
+    paste(collapse=" + ")
 
-ft[servidores, servidores := i.servidores, on=c("id_orgao_julgador", "sigla_tribunal")]
+model_formula <- parse(
+    text=glue("tramit_tmp ~ {explicative_columns}")
+ ) %>%
+    eval()
 
-ft[is.na(ft)] <- 0
-
-fit_inicial_lm <- lm(tramit_tmp ~ sigla_grau_G1 + formato_Eletrônico + procedimento_2 + procedimento_5 + procedimento_6 + procedimento_7 + servidores + ind5 + ind4 + ind6a + ind8a + ind9 + ind10 + ind11 + ind13a + ind13b + ind24 + ind25 + ind26, data = ft)
+quantiles <- list(.1, .25, .5, .75, .9)
 
 rq_models <- list()
-model_coeffs <- list(lm = coefficients(fit_inicial_lm))
 
-for (qntle in c(.1, .25, .5, .75, .9)) {
-    rq_mod <- rq(tramit_tmp ~ sigla_grau_G1 + formato_Eletrônico + procedimento_2 + procedimento_5 + procedimento_6 + procedimento_7 + servidores + ind5 + ind4 + ind6a + ind8a + ind9 + ind10 + ind11 + ind13a + ind13b + ind24 + ind25 + ind26, tau = qntle, data = ft)
-    
-    model_coeffs[[glue("rq_{qntle}")]] <- coefficients(rq_mod)
-
-    rq_models[[glue("rq_{qntle}")]] <- rq_mod
+for (q in quantiles) {
+    model <- rq(model_formula, tau = q, data = ft)
+    step_selection <- stepAIC(model)
+    rq_models[[as.character(q)]] <- step_selection
 }
 
-coeffs <- do.call(cbind, model_coeffs) %>%
-    round(3) %>%
-    as.data.frame() %>%
-    rownames_to_column("coeff")
 
-# Aplique a seleção de modelos stepwise para a frente (forward)
+fit_inicial_lm <- lm(model_formula, data = ft)
+
 lm_fit_selecionado <- stepAIC(fit_inicial_lm)
 
-rq_fit_selecionado <- stepAIC(fit_inicial_rq)
+rq_summaries <- list()
 
-# Veja o modelo final selecionado
-summary(fit_selecionado)
+for (quantile in names(rq_models)) rq_summaries[[quantile]] <- summary(rq_models[[quantile]])
