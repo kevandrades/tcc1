46a42341072205ae4c398c29bb62391734bb49d0
Kevyn Andrade <kevsdeandrade@gmail.com>
2024-03-24 08:37:38 -0300

    usar ~ . é mais direto, estabelecendo base de treino maior

--- models.R
+++ models.R
@@ -5,29 +5,19 @@ set.seed(299792458)
 ft <- fread("data/infame_filter.csv") 
 
 ft_train <- ft %>%
-    sample_n(2000)
-
-explicative_columns <- names(ft) %>%
-    setdiff("tramit_tmp") %>%
-    paste(collapse=" + ")
-
-model_formula <- parse(
-    text=glue("tramit_tmp ~ {explicative_columns}")
- ) %>%
-    eval()
+    sample_n(2200)
 
 quantiles <- list(.1, .25, .5, .75, .9)
 
 rq_models <- list()
 
 for (q in quantiles) {
-    model <- rq(model_formula, tau = q, data = ft)
+    model <- rq(tramit_tmp ~ ., tau = q, data = ft_train)
     step_selection <- stepAIC(model)
     rq_models[[as.character(q)]] <- step_selection
 }
 
-
-fit_inicial_lm <- lm(model_formula, data = ft)
+fit_inicial_lm <- lm(model_formula, data = ft_train)
 
 lm_fit_selecionado <- stepAIC(fit_inicial_lm)
 
