9f9e0100e1753105d86a4b665f8c1fa0ad050690
Kevyn Andrade <kevsdeandrade@gmail.com>
2024-04-08 00:40:11 -0300

    filtrando os TRTs durante a conversão para UTF8 (economiza memória)

--- src/data/converters.py
+++ src/data/converters.py
@@ -1,12 +1,13 @@
 BLOCKSIZE = 1024 * 1024
 
-def latin1_to_utf8(filename, out_filename = None, blocksize = BLOCKSIZE):
+def latin1_to_utf8(filename, out_filename = None):
     if not out_filename:
         out_filename = filename
     
     with open(filename, 'rb') as inf:
         with open(out_filename, 'wb') as ouf:
             while True:
-                data = inf.read(blocksize)
+                data = inf.readline()
                 if not data: break
-                ouf.write(data.decode('latin1').encode('utf-8'))
+                if 'TRT' in data:
+                    ouf.write(data.decode('latin1').encode('utf-8'))
