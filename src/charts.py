95521479c8964a45909c2a218489a53e5095830d
Kevyn Andrade <kevsdeandrade@gmail.com>
2024-04-08 00:42:31 -0300

    melhorando formatações, criando novos gráficos, renomeando tramit_tmp

--- src/charts.py
+++ src/charts.py
@@ -7,31 +7,49 @@ from plotnine import *
 
 theme_set(theme_bw)
 
-def ts_fmt(df):
-    ts_fmt = (
-        df.groupby(["ultimo_dia"])
+def ts_fmt(df, col = "ind1"):
+    ylabel = {
+        "ind1": "Casos Novos Físicos (%)",
+        "ind5": "Processos Físicos Tramitando (%)"
+    }[col]
+
+    ybreaks = ()
+
+    df_fmt = (
+        df.group_by(["ultimo_dia"])
         .agg([
-            (100 * (c.ind1 * (c.formato == "Físico")).sum() / c.ind1.sum()).alias("pct_fisicos"),
-            (100 * (c.ind1 * (c.formato == "Físico"))).sum().alias("sum_fisicos")
+            (
+                100 * (pl.col(col) * (c.formato == "Físico")).sum() /
+                pl.col(col).sum()
+            ).alias(ylabel)
         ])
     )
 
-    return (
-        ggplot(ts_fmt) +
-        aes(x = "ultimo_dia", y = "pct_fisicos") +
-        scale_y_continuous(
-            labels = ("0,00", "0,02", "0,04", "0,06")
-        ) +
+    chart = (
+        ggplot(df_fmt) +
+        aes(x = "ultimo_dia", y = ylabel) +
         scale_x_datetime(breaks=date_breaks('1 year'), labels=date_format('%Y')) +
+        scale_y_continuous(limits=(0, df_fmt[ylabel].max())) +
         geom_line() +
         theme_bw() +
-        labs(x = "Data", y = "Casos Novos Físicos (%)")
+        labs(x = "Data", y = ylabel)
     )
 
-def dist_tramit_tmp(df):
+    scales = tuple(chart.scales[1].get_breaks())
+
+    chart = (
+        chart + scale_y_continuous(
+            limits = (min(scales), max(scales)),
+            labels = [str(round(x, 2)).zfill(1).replace(".", ",") for x in scales]
+        )
+    )
+
+    return chart
+
+def dist_bx_tmp(df):
     return (
         ggplot(df)
-        + aes(x = "tramit_tmp", y = after_stat('density'))
+        + aes(x = "bx_tmp", y = after_stat('density'))
         + geom_histogram(fill = "#28ac84", bins = 40)
         + geom_density()
         + xlim(0, 8e3)
@@ -63,7 +81,7 @@ def fmt_tmp(df):
 def grau_tempo(df):
     return (
         ggplot(df)
-        + aes(x = "grau", y = "tramit_tmp", fill = "grau")
+        + aes(x = "grau", y = "bx_tmp", fill = "grau")
         + geom_boxplot()
         + labs(x = "Grau de Jurisdição", y = "Tempo de tramitação (dias)")
         + scale_fill_manual(values = ["#484484", "#28ac84"])
@@ -78,7 +96,7 @@ def grau_tempo(df):
 def originario_tempo(df):
     return (
         ggplot(df)
-        + aes(x = "originario", y = "tramit_tmp", fill = "originario")
+        + aes(x = "originario", y = "bx_tmp", fill = "originario")
         + geom_boxplot()
         + scale_fill_manual(values = ["#484484", "#28ac84"])
         + guides(fill=False)
@@ -102,7 +120,7 @@ def procedimento_tempo(df):
                 .cast(pl.Categorical).cat.set_ordering("physical")
             )
         ) +
-        aes(y = "tramit_tmp", x = "procedimento", fill = "procedimento") +
+        aes(y = "bx_tmp", x = "procedimento", fill = "procedimento") +
         geom_boxplot() +
         labs(y = "Tempo de tramitação (dias)", x="", fill="") +
         theme(
