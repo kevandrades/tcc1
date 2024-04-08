b7b4fb9426b48c26a3fdbadd781c21649d3342c3
Kevyn Andrade <kevsdeandrade@gmail.com>
2024-04-08 00:41:12 -0300

    corrigindo bugs, salvando header, lendo linha a linha

--- src/data/converters.py
+++ src/data/converters.py
@@ -6,8 +6,10 @@ def latin1_to_utf8(filename, out_filename = None):
     
     with open(filename, 'rb') as inf:
         with open(out_filename, 'wb') as ouf:
+            header = inf.readline()
+            ouf.write(header.decode('latin1').encode('utf-8'))
             while True:
                 data = inf.readline()
                 if not data: break
-                if 'TRT' in data:
+                if b'TRT' in data:
                     ouf.write(data.decode('latin1').encode('utf-8'))
