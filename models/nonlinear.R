4666950448c9e6402e5cecd9fff45424acc80f54
Kevyn Andrade <kevsdeandrade@gmail.com>
2024-05-25 20:14:55 -0300

    criando arquivo específico para as variáveis não-lineares

---
+++ models/nonlinear.R
@@ -0,0 +1,10 @@
+source("models/models.R")
+options(scipen=999)
+
+nonlinear <- rq(bx_tmp ~ ind5 + ind4 + ind10 + ind9 + ind6a + ind8a + I(ind11^2) + I(ind13a^2) + I(ind13b^2) + I(ind26^2) + I(ind24^2) + I(ind25^2), tau = .5, data = train_data) %>%
+    stepAIC()
+
+nonlinear %>% model_to_df(
+    caption="Modelo de regressão quantílica ($\\tau$ = {tau}) com variáveis expressões quadráticas e seleção {italic_stepwise}",
+    label="tab:qr_median_nonlinear_"
+)
