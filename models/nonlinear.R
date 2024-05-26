047664f393a3578baf24b9d24e4180a98667789a
Kevyn Andrade <kevsdeandrade@gmail.com>
2024-05-26 16:32:31 -0300

    substituindo nome da função

--- models/nonlinear.R
+++ models/nonlinear.R
@@ -4,7 +4,7 @@ options(scipen=999)
 qr_nonlinear <- rq(bx_tmp ~ ind5 + ind4 + ind10 + ind9 + ind6a + ind8a + I(ind11^2) + I(ind13a^2) + I(ind13b^2) + I(ind26^2) + I(ind24^2) + I(ind25^2), tau = .5, data = train_data) %>%
     stepAIC()
 
-qr_nonlinear %>% model_to_df(
+qr_nonlinear %>% qr_model_to_df(
     caption="Regressão quantílica ($\\tau$ = {tau}) com variáveis expressões quadráticas e seleção {italic_stepwise}",
     label="tab:qr_median_nonlinear_"
 )
