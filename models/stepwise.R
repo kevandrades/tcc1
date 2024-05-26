674fd7852a776e5704ca01e78e31efc460ecf308
Kevyn Andrade <kevsdeandrade@gmail.com>
2024-05-26 18:51:01 -0300

    removendo objeto do outro arquivo

--- models/stepwise.R
+++ models/stepwise.R
@@ -4,22 +4,17 @@ fml <- bx_tmp ~ .
 
 quantiles <- c(.1, .25, .5, .75, .9)
 
-qr_linear <- with(list(), {
-    qr_linear <- list()
+step1 <- with(list(), {
+    step1 <- list()
     
     for (q in quantiles) {
-        qr_linear[[as.character(q)]] <- stepAIC(rq(fml, tau = q, data = train_data))    
+        qr_linear[[as.character(q)]] <- stepwiseAIC(rq(fml, tau = q, data = train_data))    
     }
 
-    qr_linear
+    step1
 })
 
 for (model in qr_linear) qr_model_to_df(model,
     caption = "Regressão quantílica ($\\tau$ = {tau}) selecionado por {italic_stepwise}",
     label = "tab:qr_woutinter_"
-)
-
-qr_median_nonlinear <- rq(
-  bx_tmp ~ sigla_grau_G2 + procedimento_6 + formato_Físico + ind5 + ind4 + ind10 + ind9 + ind6a,
-  tau = .5, data = train_data
 )
