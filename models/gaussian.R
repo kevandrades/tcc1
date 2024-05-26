3b80d7c049918fd5dec44062966ca4a31f0fbdb7
Kevyn Andrade <kevsdeandrade@gmail.com>
2024-05-26 18:49:54 -0300

    substituindo nome da função

--- models/gaussian.R
+++ models/gaussian.R
@@ -2,7 +2,7 @@ source("models/models.R")
 
 gr_full <- lm(bx_tmp ~ ., data = train_data)
 
-gr_selection <- stepAIC(gr_full)
+gr_selection <- stepwiseAIC(gr_full)
 
 gr_interactions <- with(gr_selection, {
     categorical <- names(coefficients) %>% .[!str_detect(., "(ind|Intercept)")] %>% paste(collapse = " + ")
@@ -14,11 +14,11 @@ gr_interactions <- with(gr_selection, {
     lm(as.formula(
         glue("bx_tmp ~ {categorical} + {individual} + {interact}")
     ), data = train_data) %>%
-    stepAIC()
+    stepwiseAIC()
 })
 
 gr_nonlinear <- lm(bx_tmp ~ ind5 + ind4 + ind10 + ind9 + ind6a + ind8a + I(ind11^2) + I(ind13a^2) + I(ind13b^2) + I(ind26^2) + I(ind24^2) + I(ind25^2), data = train_data) %>%
-    stepAIC()
+    stepwiseAIC()
 
 gr_model_to_df(
     gr_full,
