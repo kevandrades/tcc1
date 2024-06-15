16904fee35192cf4f41383c2071d0f473d4e9eee
Kevyn Andrade <kevsdeandrade@gmail.com>
2024-06-15 16:48:38 -0300

    removendo trechos não usados no seletor

--- models/gaussian.R
+++ models/gaussian.R
@@ -1,25 +1,36 @@
 source("models/models.R")
+source("models/correlator.R")
+source("models/pval_selector.R")
 
-gr_selection <- stepwiseAIC(lm(bx_tmp ~ ., data = train_data))
+gaussian_mdl1 <- stepwiseAIC(lm(bx_tmp ~ ., data = train_data)) %>% 
+    select_gaussian_with_pval(sig.level=.1)
 
 gr_model_to_df(
-    gr_selection,
+    gaussian_mdl1,
     caption = "Regressão gaussiana com seleção \\textit{stepwise} usando AIC",
     label = "tab:gr_selection"
 )
 
-gr_interactions <- with(gr_selection, {
-    categorical <- names(coefficients) %>% .[!str_detect(., "(ind|Intercept)")] %>% paste(collapse = " + ")
-    inds <- names(coefficients) %>% .[str_detect(., "ind")]
+gr_interactions <- with(gaussian_mdl1, {
+      corrs <- correlator(gaussian_mdl1, train_data)
 
-    individual <- paste(inds, collapse=' + ')
-    interact <- paste(combn(inds, 2, paste, collapse=':'), collapse=' + ')
+      numeric_cols <- names(coefficients) %>% .[str_detect(., "ind")]
 
-    lm(as.formula(
-        glue("bx_tmp ~ {categorical} + {individual} + {interact}")
-    ), data = train_data) %>%
-    stepwiseAIC()
-})
+      full_cols <- names(coefficients) %>% .[!str_detect(., "Intercept")]
+
+      nonlinears <- "I(ind11^2) + I(ind13a^2) + I(ind13b^2) + I(ind26^2) + I(ind24^2) + I(ind25^2)"
+
+      fml <- paste(c(
+          glue("bx_tmp ~ {paste(full_cols, collapse='+')}"),
+          corrs$unique_key,
+          nonlinears
+      ), collapse=" + ")
+
+      result <- stepwiseAIC(lm(as.formula(fml), data = train_data), direction="both") %>%
+        select_gaussian_with_pval(sig.level = .1)
+
+      return(result)
+  })
 
 
 gr_model_to_df(
@@ -27,19 +38,4 @@ gr_model_to_df(
     caption = "Regressão gaussiana com interações",
     label = "tab:gr_selection_interaction"
 )
-
-gr_nonlinear <- lm(bx_tmp ~ ind5 + ind4 + ind10 + ind9 + ind6a + ind8a + I(ind11^2) + I(ind13a^2) + I(ind13b^2) + I(ind26^2) + I(ind24^2) + I(ind25^2), data = train_data) %>%
-    stepwiseAIC()
-
-
-gr_model_to_df(
-    gr_nonlinear,
-    caption = "Regressão gaussiana com variáveis não-lineares",
-    label = "tab:gr_selection_nonlinear"
-)
-
-gr_minimalist <- lm(formula = bx_tmp ~ ind5 + ind4 + ind10 + ind9 + ind6a, 
-    data = train_data)
-
-gr_model_to_df(gr_minimalist, caption="Regressão gaussiana com variáveis quantitativas mais relevantes", label="tab:gr_minimalist")
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
