23a6dd524f441fe32152ccd4dd50e4793fb590d6
Kevyn Andrade <kevsdeandrade@gmail.com>
2024-06-15 16:48:20 -0300

    criando arquivo unificado

---
+++ models/other_models.R
@@ -0,0 +1,141 @@
+source("models/models.R")
+source("models/selector.R")
+options(scipen=6, OutDec=",")
+
+quantiles <- c(.1, .25, .5, .75, .9)
+
+final <- tibble(Variável = "Intercepto")
+
+aic_linear <- c()
+aic_nonlinear <- c()
+bic_linear <- c()
+bic_nonlinear <- c()
+rho_linear <- c()
+rho_nonlinear <- c()
+
+bic_estimate <- function(object) {
+#    ll   <- if(isNamespaceLoaded("stats4")) stats4::logLik else logLik
+    
+    lls <- stats4::logLik(object)
+    nos <- with(object, nrow(x))
+    
+    -2 * as.numeric(lls) + log(nos) * attr(lls, "df")
+}
+
+
+for (tau in quantiles) {
+    mdl1 <- candidate1(tau)
+    mdl2 <- candidate2(tau, mdl1)
+
+    aic_linear <- c(aic_linear, AIC(mdl1))
+    aic_nonlinear <- c(aic_nonlinear, AIC(mdl2))
+    bic_linear <- c(bic_linear, bic_estimate(mdl1))
+    bic_nonlinear <- c(bic_nonlinear, bic_estimate(mdl2))
+    rho_linear <- c(rho_linear, with(mdl1, rho))
+    rho_nonlinear <- c(rho_nonlinear, with(mdl2, rho))
+    
+
+    mdl1_df <- qr_model_to_df(mdl1) %>%
+        select(Variável, Coeficiente) %>%
+        as_tibble()
+
+    colnames(mdl1_df) <- c("Variável", paste0("1 (tau = ", tau, ")"))
+    
+    mdl2_df <- qr_model_to_df(mdl2) %>%
+        select(Variável, Coeficiente) %>%
+        as_tibble()
+    
+    colnames(mdl2_df) <- c("Variável", paste0("2 (tau = ", tau, ")"))
+
+    mdl_df <- full_join(mdl1_df, mdl2_df)
+
+    final <- full_join(final, mdl_df)
+}
+
+sort_vars <- c(
+    "Intercepto",
+    "Grau",
+    "Formato",
+    "Procedimento - Conhecimento não criminal",
+    "Procedimento - Pré-processual",
+    "Procedimento - Outros",
+    "Tramitando",
+    "Suspensos e Sobrestados",
+    "Despachos",
+    "Decisões",
+    "Conclusos",
+    "Rec. Interno Julgado",
+    "Liminares indeferidas",
+    "Julgamentos",
+    "Rec. Interno Pendente",
+    "Despachos:Liminares indeferidas",
+    "Liminares indeferidas:Julgamentos",
+    "Suspensos e Sobrestados:Decisões",
+    "Rec. Interno Pendente:Julgamentos",
+    "Julgamentos:Tramitando",
+    "Suspensos e Sobrestados:Rec. Interno Julgado",
+    "Suspensos e Sobrestados:Conclusos",
+    "Tramitando:Suspensos e Sobrestados",
+    "Rec. Interno Pendente:Conclusos",
+    "Suspensos e Sobrestados:Despachos",
+    "Tramitando:Despachos",
+    "Suspensos e Sobrestados:Tramitando"
+)
+
+final <- final %>%
+    mutate(Variável = factor(final$Variável, levels=sort_vars)) %>%
+    arrange(Variável)
+
+nvars <- final %>% select(-Variável) %>% (function(x) !is.na(x)) %>% colSums()
+
+a <- with(final, {
+    data.frame(
+    quantiles = names(nvars) %>% str_remove_all("(.+ = )|\\)") %>% str_replace(",", ".") %>% as.numeric(),
+    type = names(nvars) %>% str_remove_all(" .+") %>% c("1" = "linear", "2" = "nonlinear")[.],
+    nvars = nvars
+) %>%
+    pivot_wider(names_from=type, values_from=nvars) %>%
+    mutate(
+        aic_linear, bic_linear,
+        aic_nonlinear, bic_nonlinear,
+        rho_linear, rho_nonlinear,
+        nlinear = as.integer(linear), nnonlinear = as.integer(nonlinear)
+    ) %>%
+    select(
+        q = quantiles,
+        aic_linear, aic_nonlinear,
+        bic_linear, bic_nonlinear,
+        rho_linear, rho_nonlinear,
+        nlinear, nnonlinear
+    ) %>%
+    mutate(
+        corresp = c(
+            sum(!is.na(`1 (tau = 0,1)`) & !is.na(`2 (tau = 0,1)`)),
+            sum(!is.na(`1 (tau = 0,25)`) & !is.na(`2 (tau = 0,25)`)),
+            sum(!is.na(`1 (tau = 0,5)`) & !is.na(`2 (tau = 0,5)`)),
+            sum(!is.na(`1 (tau = 0,75)`) & !is.na(`2 (tau = 0,75)`)),
+            sum(!is.na(`1 (tau = 0,9)`) &   !is.na(`2 (tau = 0,9)`))
+        )
+    )
+})
+
+
+xtable::xtable(
+    a %>% lapply(as.integer) %>%
+    as.data.frame() %>%
+    mutate(q = quantiles)
+) %>%
+    print(include.rownames = FALSE)
+
+final %>%
+    select(-Variável) %>%
+    mutate_all(function(x) str_replace_all(x, "^.*$", "\\checkmark")) %>%
+    mutate(Variável = final$Variável, .before=`1 (tau = 0,1)`) %>%
+    xtable::xtable(digits=2) %>%
+    print(digits=2) 
+
+
+final %>%
+    select(Variável, `1 (tau = 0,1)`, `1 (tau = 0,25)`, `1 (tau = 0,5)`, `2 (tau = 0,75)`, `2 (tau = 0,9)`) %>%
+    xtable::xtable(digits=3) %>%
+    print(digits=3)
