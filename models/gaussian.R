87b5835e5554dfa7a7482d8fa35b97f769a5f256
Kevyn Andrade <kevsdeandrade@gmail.com>
2024-06-16 08:50:42 -0300

    atualizando

--- models/gaussian.R
+++ models/gaussian.R
@@ -1,18 +1,26 @@
 source("models/models.R")
 source("models/correlator.R")
 source("models/pval_selector.R")
+library(xtable)
 
-gaussian_mdl1 <- stepwiseAIC(lm(bx_tmp ~ ., data = train_data)) %>% 
-    select_gaussian_with_pval(sig.level=.1)
-
-gr_model_to_df(
-    gaussian_mdl1,
-    caption = "Regressão gaussiana com seleção \\textit{stepwise} usando AIC",
-    label = "tab:gr_selection"
+options(
+    xtable.sanitize.colnames.function = function(x) paste("\\textbf{", x, "}", sep = "")
 )
 
-gr_interactions <- with(gaussian_mdl1, {
-      corrs <- correlator(gaussian_mdl1, train_data)
+lbl_mdl1 <- "tab:gr_selection"
+lbl_mdl2 <- "tab:gr_selection_interaction"
+
+gr_mdl1 <- stepwiseAIC(lm(bx_tmp ~ ., data = train_data)) %>% 
+    select_gaussian_with_pval(sig.level=.05)
+
+gr_model_to_df(
+    gr_mdl1,
+    caption = "Regressão gaussiana com seleção \\textit{stepwise} usando AIC",
+    label = lbl_mdl1
+)
+
+gr_mdl2 <- with(gr_mdl1, {
+      corrs <- correlator(gr_mdl1, train_data)
 
       numeric_cols <- names(coefficients) %>% .[str_detect(., "ind")]
 
@@ -27,15 +35,30 @@ gr_interactions <- with(gaussian_mdl1, {
       ), collapse=" + ")
 
       result <- stepwiseAIC(lm(as.formula(fml), data = train_data), direction="both") %>%
-        select_gaussian_with_pval(sig.level = .1)
+        select_gaussian_with_pval(sig.level = .05)
 
       return(result)
   })
 
 
 gr_model_to_df(
-    gr_interactions,
+    gr_mdl2,
     caption = "Regressão gaussiana com interações",
-    label = "tab:gr_selection_interaction"
+    label = lbl_mdl2
 )
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+
+data.frame(
+    Modelo = paste0("\\ref{", c(lbl_mdl1, lbl_mdl2), "}"),
+    AIC = c(AIC(gr_mdl1), AIC(gr_mdl2)) %>% prettyNum(big.mark = "."),
+    BIC = c(BIC(gr_mdl1), BIC(gr_mdl2)) %>% prettyNum(big.mark = ".")
+) %>%
+    xtable::xtable(
+        align = "cc|cc",
+        label = "tab:aic_bic_gaussianos",
+        caption = "AIC e BIC para os modelos gaussianos"
+    ) %>%
+    print(
+        include.rownames=F,
+        caption.placement = "top",
+        sanitize.text.function = identity
+    )
